2cf0c9f87aec80a8e49ce645d84d50514fbd1818
ludooo2204 <vachon.ludovic@gmail.com>
2021-03-26 21:56:17 +0100

    design !started

--- App.js
+++ App.js
@@ -13,8 +13,8 @@ gestion des parametres de config
 
 */
 
-import {transform} from '@babel/core';
-import React, {useState, useEffect} from 'react';
+import { transform } from '@babel/core';
+import React, { useState, useEffect } from 'react';
 
 import {
   Text,
@@ -23,6 +23,7 @@ import {
   View,
   TimePickerAndroid,
   ToolbarAndroid,
+  StyleSheet
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Icon2 from 'react-native-vector-icons/MaterialIcons';
@@ -52,27 +53,27 @@ class App extends React.Component {
     this.resume = this.resume.bind(this);
   }
   strings = new LocalizedStrings({
-		"en-US":{
-		  how:"How do you want your egg today?",
-		  boiledEgg:"Boiled egg",
-		  softBoiledEgg:"Soft-boiled egg",
-		  choice:"How to choose the egg"
-		},
-		en:{
-		  how:"How do you want your egg today?",
-		  boiledEgg:"Boiled egg",
-		  softBoiledEgg:"Soft-boiled egg",
-		  choice:"How to choose the egg"
-		},
-		fr: {
-		  how:"ca va mon?",
-		  boiledEgg:"Uovo sodo",
-		  softBoiledEgg:"Uovo alla coque",
-		  choice:"Come scegliere l'uovo"
-		}
-	   });
+    "en-US": {
+      how: "How do you want your egg today?",
+      boiledEgg: "Boiled egg",
+      softBoiledEgg: "Soft-boiled egg",
+      choice: "How to choose the egg"
+    },
+    en: {
+      how: "How do you want your egg today?",
+      boiledEgg: "Boiled egg",
+      softBoiledEgg: "Soft-boiled egg",
+      choice: "How to choose the egg"
+    },
+    fr: {
+      how: "ca va mon?",
+      boiledEgg: "Uovo sodo",
+      softBoiledEgg: "Uovo alla coque",
+      choice: "Come scegliere l'uovo"
+    }
+  });
 
-  
+
   startWhiteTimer() {
     console.log('startTimer!!!');
     this.setState({
@@ -105,7 +106,7 @@ class App extends React.Component {
   }
   start() {
     console.log('start');
-    this.setState({isWhiteTurn: true, started: true, isOn: true});
+    this.setState({ isWhiteTurn: true, started: true, isOn: true });
     this.handleTimer();
   }
   stopWhiteTimer() {
@@ -115,18 +116,18 @@ class App extends React.Component {
     clearInterval(this.blackTimer);
   }
   resetTimer() {
-    this.setState({timeWhite: 0, isOn: false});
+    this.setState({ timeWhite: 0, isOn: false });
   }
 
   pauseTimer() {
     console.log(this.state.started && this.state.isOn ? 'pause' : 'reprise');
     if (this.state.isOn) {
-      this.setState({isOn: false});
+      this.setState({ isOn: false });
       this.stopBlackTimer();
       this.stopWhiteTimer();
     } else {
       // console.log("reprise")
-      this.setState({isOn: true});
+      this.setState({ isOn: true });
       this.resume();
     }
   }
@@ -159,7 +160,7 @@ class App extends React.Component {
       this.stopBlackTimer();
     }
 
-    this.setState({isWhiteTurn: !this.state.isWhiteTurn});
+    this.setState({ isWhiteTurn: !this.state.isWhiteTurn });
   }
   render() {
     return (
@@ -176,53 +177,56 @@ class App extends React.Component {
               justifyContent: 'center',
               alignItems: 'center',
             },
-            {transform: [{rotate: '180deg'}]},
+            { transform: [{ rotate: '180deg' }] },
           ]}
           onPress={this.handleTimer}>
-          <Text style={{color: 'black', fontSize: 50}}>
-		  
+          <Text style={{ color: 'black', fontSize: 50 }}>
+
             {this.state.timeWhite / 1000 < 3600
               ? new Date(this.state.timeWhite).toISOString().substr(14, 5)
               : "plus d'une heure!"}
           </Text>
         </Pressable>
-        <View style={{flexDirection: 'row', flex: 1}}>
+        <View style={
+          this.state.started?(this.state.isWhiteTurn?({ flexDirection: 'row', flex: 1,backgroundColor: 'white', }):({ flexDirection: 'row', flex: 1,backgroundColor: 'black', })):({ flexDirection: 'row', flex: 1,backgroundColor: 'grey', })
+          // this.state.isWhiteTurn?({ flexDirection: 'row', flex: 1,backgroundColor: 'white', }):({ flexDirection: 'row', flex: 1,backgroundColor: 'black', })
+          }>
           <View
             style={{
-              backgroundColor: 'black',
+              // backgroundColor: 'black',
               flex: 1,
               justifyContent: 'center',
               alignItems: 'center',
             }}>
-            <Icon name="cog" size={50} color="#fff" />
+            {this.state.isWhiteTurn?<Icon name="cog" size={50} color="#000" />:<Icon name="cog" size={50} color="#fff" />}
           </View>
 
           {!this.state.started ? (
             <Pressable
               style={{
-                backgroundColor: 'black',
+                // backgroundColor: 'black',
                 flex: 1,
                 justifyContent: 'center',
                 alignItems: 'center',
               }}
               onPress={this.start}>
-              <Icon name="caret-right" size={70} color="#fff" />
+              {this.state.isWhiteTurn?<Icon name="caret-right" size={70} color="#000" />:<Icon name="caret-right" size={70} color="#fff" />}
             </Pressable>
           ) : this.state.isOn ? (
             <Pressable
               style={{
-                backgroundColor: 'black',
+                // backgroundColor: 'black',
                 flex: 1,
                 justifyContent: 'center',
                 alignItems: 'center',
               }}
               onPress={this.pauseTimer}>
-              <Icon2 name="pause-presentation" size={70} color="#fff" on />
+              {this.state.isWhiteTurn?<Icon2 name="pause-presentation" size={70} color="#000" on />:<Icon2 name="pause-presentation" size={70} color="#fff" on />}
             </Pressable>
           ) : (
             <Pressable
               style={{
-                backgroundColor: 'black',
+                // backgroundColor: 'black',
                 flex: 1,
                 justifyContent: 'center',
                 alignItems: 'center',
@@ -230,25 +234,30 @@ class App extends React.Component {
               onPress={this.pauseTimer}>
               <Animatable.Text
                 animation="flash"
+                easing="linear"
                 iterationCount="infinite"
-                duration={400}>
-                <Icon2 name="pause-presentation" size={80} color="#fff" on />
+                direction="alternate"
+                // transition="backgroundColor"
+                duration={2000}
+                >
+                {this.state.isWhiteTurn?<Icon2 name="pause-presentation" size={80} color="#000" on />:<Icon2 name="pause-presentation" size={80} color="#fff" on />}
               </Animatable.Text>
             </Pressable>
           )}
-          {/* black */}
-          <Pressable
+            <Pressable
             style={{
-              backgroundColor: 'black',
+              // backgroundColor: 'black',
               flex: 1,
               justifyContent: 'center',
               alignItems: 'center',
             }}>
             {/* <Text style={{color: 'black', fontSize: 40}}>restart</Text> */}
 
-            <Icon name="refresh" size={50} color="#fff" />
+            {this.state.isWhiteTurn?<Icon name="refresh" size={50} color="#000" />:<Icon name="refresh" size={50} color="#fff" />}
           </Pressable>
         </View>
+
+        {/* black */}
         <Pressable
           style={[
             {
@@ -259,13 +268,13 @@ class App extends React.Component {
             },
           ]}
           onPress={this.handleTimer}>
-          <Text style={{color: 'white', fontSize: 50}}>
-            {this.state.started ? 'ON' : 'OFF'}
-            {this.state.isWhiteTurn ? 'white' : 'black'}
+          <Text style={{ color: 'white', fontSize: 50 }}>
+            {/* {this.state.started ? 'ON' : 'OFF'} */}
+            {/* {this.state.isWhiteTurn ? 'white' : 'black'} */}
             {this.state.timeBlack / 1000 < 3600
               ? new Date(this.state.timeBlack).toISOString().substr(14, 5)
               : "plus d'une heure!"}
-			  {"\n"} {this.strings.how}
+            {/* {"\n"} {this.strings.how} */}
           </Text>
         </Pressable>
       </View>
@@ -274,3 +283,19 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+
+const styles = StyleSheet.create({
+  whiteStyle: {
+    color: 'black',
+    backgroundColor:"white"
+  },
+  blackStyle: {
+    color: 'white',
+    backgroundColor:"black"
+  },
+  red: {
+    color: 'red',
+  },
+});
